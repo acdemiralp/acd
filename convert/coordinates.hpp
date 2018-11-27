9ad35b87293b63edaf2f46b081a607a8046b01f1
Ali Can Demiralp <demiralpali@gmail.com>
2018-11-27 20:08:06 +0100

    Fixed formatting.

--- convert/coordinates.hpp
+++ convert/coordinates.hpp
@@ -10,7 +10,7 @@ type to_spherical(const type& cartesian)
 {
   type spherical;
   spherical[0] = std::sqrt (std::pow(cartesian[0], 2) + std::pow(cartesian[1], 2) + std::pow(cartesian[2], 2));
-  spherical[1] = std::atan2(cartesian[1] , cartesian [0]);
+  spherical[1] = std::atan2(cartesian[1] , cartesian[0]);
   spherical[2] = std::acos (cartesian[2] / spherical[0]);
   return spherical;
 }
