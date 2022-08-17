d2149a640f05d9e16e701d3602441b1410312b0e
Ali Can Demiralp <demiralpali@gmail.com>
2022-08-17 05:56:00 +0200

    Update constexpr_for.hpp

--- include/acd/constexpr_for.hpp
+++ include/acd/constexpr_for.hpp
@@ -10,7 +10,7 @@ namespace acd
 template <auto begin, auto end, auto increment, typename function_type>
 constexpr void constexpr_for      (function_type&& function)
 {
-  if constexpr (begin < end)
+  if constexpr (increment > 0 && begin < end || increment < 0 && begin > end)
   {
     function(std::integral_constant<decltype(begin), begin>());
     constexpr_for<begin + increment, end, increment>(function);
