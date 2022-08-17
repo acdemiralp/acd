66a4c867612a6db2311d2816cdc530e0bfedb532
Ali Can Demiralp <demiralpali@gmail.com>
2022-08-17 05:56:30 +0200

    Create factorial.hpp

---
+++ include/acd/factorial.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <type_traits>
+#include <utility>
+
+namespace fd
+{
+template <typename type, type n, typename = std::make_integer_sequence<type, n>>
+struct factorial_t;
+template <typename type, type n, type... sequence>
+struct factorial_t<type, n, std::integer_sequence<type, sequence...>> : std::integral_constant<type, (... * (sequence + 1))> {};
+
+template <typename type, type n>
+inline constexpr type factorial_v = factorial_t<type, n>::value;
+
+template <typename type>
+constexpr type factorial(const type n)
+{
+  type result{1};
+  for (auto i = type{2}; i <= n; ++i)
+    result *= i;
+  return result;
+}
+}
